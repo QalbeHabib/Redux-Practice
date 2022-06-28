b06b3d793b598f04d06d61c690829304f26fae10
QalbeHabib <rana.habib313@gmail.com>
2022-06-29 00:48:38 +0500

    Redux Tool kit added with the same functionality

--- src/App.js
+++ src/App.js
@@ -1,40 +1,82 @@
 import React from 'react';
 import './style.css';
+
+// Redux Method
 import { useSelector, useDispatch } from 'react-redux';
+
+// redux-Toolkit Method
+import { actions } from './store/redux_Toolkit.js';
 
 export default function App() {
   const counter = useSelector((state) => state.counter);
+  const name = useSelector((state) => state.name);
   const dispatch = useDispatch();
 
- 
   return (
+    // Simple Redux Method
+    // <div>
+    //   <h1>Hello StackBlitz!</h1>
+    //   <p>Start editing to see some magic happen : {counter} </p>
+    //   <button
+    //     onClick={() => {
+    //       dispatch({ type: 'INC' });
+    //     }}
+    //   >
+    //     Increment
+    //   </button>
+    //   <button
+    //     onClick={() => {
+    //       dispatch({ type: 'DEC' });
+    //     }}
+    //   >
+    //     Decrement
+    //   </button>
+    //   <button
+    //     onClick={() => {
+    //       dispatch({ type: 'ADD', payload: 10 });
+    //     }}
+    //   >
+    //     Add By 10
+    //   </button>
+    //   <button
+    //     onClick={() => {
+    //       dispatch({ type: 'RED', payload: 10 });
+    //     }}
+    //   >
+    //     Dec By 10
+    //   </button>
+    // </div>
+
+    // Redux Toolkit
     <div>
       <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen : {counter} </p>
+      <p>
+        Start editing to see some magic happen : {counter} {name}{' '}
+      </p>
       <button
         onClick={() => {
-          dispatch({ type: 'INC' });
+          dispatch(actions.increment(2));
         }}
       >
         Increment
       </button>
       <button
         onClick={() => {
-          dispatch({ type: 'DEC' });
+          dispatch(actions.decrement());
         }}
       >
         Decrement
       </button>
       <button
         onClick={() => {
-          dispatch({ type: 'ADD', payload: 10 });
+          dispatch(actions.addBy(10));
         }}
       >
         Add By 10
       </button>
       <button
         onClick={() => {
-          dispatch({ type: 'RED', payload: 10 });
+          dispatch(actions.decBy(10));
         }}
       >
         Dec By 10
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './store/store';
+// Simple Redux Store
+// import { store } from './store/Redux_Store.js';
+
+// Redux Toolkit store
+import { store } from './store/redux_Toolkit';
 import App from './App';
 
 const rootElement = document.getElementById('root');
